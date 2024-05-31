3c6e3d2531343e4ce81e81c19ee581a0d13457ea
TruongDangQuang <quangtruong452001@gmail.com>
2024-05-31 10:41:34 +0700

    Update: config env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3002;
   app.enableCors({ origin: '*' });
-  await app.listen(3000);
+  console.log(`Server running on http://localhost:${port}`);
+  await app.listen(port);
 }
 bootstrap();
